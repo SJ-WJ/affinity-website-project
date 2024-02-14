392ed70433a88b63016b50b8de8bfa253b874a9e
Jindo Kim <jindokim.kor@gmail.com>
2024-02-14 14:05:11 -0500

    fix image loading

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Logo from '../images/logo.png';
 
 const Navbar = () => {
   return (
     <nav class="relative container mx-auto p-6 bg-slate-300 max-w-full">
       {/* Flex Container */}
       <div class="flex items-center justify-center">
-        <img src={"../images/logo.png"} alt=""/>
+      <img src={Logo} alt="Logo" />
 
         <div class="space-x-8 font-bold lg:flex">
           <a href="" class="hover:text-cyan-500">What We Do</a>
@@ -14,7 +15,6 @@ const Navbar = () => {
           <a href="" class="hover:text-cyan-500">Out There</a>
           <a href="" class="hover:text-cyan-500">Donate</a>
         </div>
-
       </div>
     </nav>
   );
