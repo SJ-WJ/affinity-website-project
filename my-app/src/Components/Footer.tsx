e02647e04194a83f03866eadcad7edb3f84cf861
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2024-02-16 09:58:26 -0700

    Starting to build out the Footer component

---
+++ my-app/src/Components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-300 text-black pt-12 bottom-0 w-full">
+      <div className="container mx-auto px-5 pb-10 flex flex-col md:flex-row justify-between flex-grow items-start">
+        
+        {/* Contact Info */}
+        <div className="md:order-1 md:mr-20 flex flex-col space-y-4 flex-grow">
+          <div className="font-bold">Affinity Mentorship</div>
+          <div>14058 Richmond Road Calgary, Alberta T3E 7Y7</div>
+          <div>403-200-9766</div>
+          <div>Charity Registration # 81705 1394 RR0001</div>
+        </div>
+
+        {/* Footer Menu */}
+        <div className="md:order-2 flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-20 text-xl md:text-base flex-grow">
+          <div className="flex flex-col space-y-3">
+            <a href="#about">About</a>
+            <a href="#jobs">Jobs</a>
+            <a href="#press">Press</a>
+            <a href="#blog">Blog</a>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- my-app/src/Components/MainPage.jsx
+++ my-app/src/Components/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer.tsx";
 
 const MainPage = () => {
   return (
     <div>
       <Header />
       <p>MainPage</p>
+      <Footer />
     </div>
   );
 };
