54ae797a01d9686117326fbb5318036e86f23042
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2024-03-02 22:58:22 -0700

    Edit footer component

--- my-app/src/Components/Shared/Footer/Footer.tsx
+++ my-app/src/Components/Shared/Footer/Footer.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import facebookIcon from '../images/facebook.svg';
-import linkedinIcon from '../images/linkedin.svg';
-import youtubeIcon from '../images/youtube.svg';
-import { FaFacebook, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
+import { FaFacebook, FaLinkedinIn, FaInstagram , FaYoutube } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   return (
@@ -11,7 +8,7 @@ const Footer: React.FC = () => {
         
         {/* Contact Info */}
         <div className="md:order-1 md:mr-20 flex flex-col space-y-4">
-          <div className="font-bold text-fawn">Affinity Mentorship</div>
+          <div className="font-bold text-fawn">Affinity Mentorship Foundation (AMF)</div>
           <div>14058 Richmond Road Calgary, Alberta T3E 7Y7</div>
           <div>403-200-9766</div>
           <div>Charity Registration # 81705 1394 RR0001</div>
@@ -22,7 +19,6 @@ const Footer: React.FC = () => {
           <div className="flex flex-col space-y-3">
             <a href="#about" className="hover:text-fawn">About</a>
             <a href="#jobs" className="hover:text-fawn">Jobs</a>
-            <a href="#press" className="hover:text-fawn">Press</a>
             <a href="#blog" className="hover:text-fawn">Blog</a>
           </div>
         </div>
@@ -34,6 +30,9 @@ const Footer: React.FC = () => {
           </div>
           <div className="text-fawn hover:text-mint-green cursor-pointer">
             <FaLinkedinIn className="w-8 h-8" />
+          </div>
+          <div className="text-fawn hover:text-mint-green cursor-pointer">
+            <FaInstagram className="w-8 h-8" />
           </div>
           <div className="text-fawn hover:text-mint-green cursor-pointer">
             <FaYoutube className="w-8 h-8" />
