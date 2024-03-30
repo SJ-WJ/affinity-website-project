0e88625453ccff96611cf072b36ee1892d2802a4
Dahyun (Dany) <iamgo910@gmail.com>
2024-03-30 14:03:53 -0400

    add banner for our-mission page
    Add banner & title

--- my-app/src/Components/Pages/WhoWeAre/OurMission/OurMission.tsx
+++ my-app/src/Components/Pages/WhoWeAre/OurMission/OurMission.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+
+import "./OurMission.css";
+import bannerImage from "../../../../images/ai-mentorship-image-1.jpg";
 
 const OurMission: React.FC = () => {
-    return (
-        <div>
-            <main className="our-mission-content">
-                <h2>Our Mission</h2>
-            </main>
+  return (
+    <div>
+      <main className="our-mission-content">
+        <div className="banner-wrapper">
+          <div className="banner-image">
+            <div className="banner-overlay">
+              <div className="banner-title">Our Mission</div>
+            </div>
+          </div>
         </div>
-    );
+        <h2>Our Mission</h2>
+      </main>
+    </div>
+  );
 };
 
 export default OurMission;
